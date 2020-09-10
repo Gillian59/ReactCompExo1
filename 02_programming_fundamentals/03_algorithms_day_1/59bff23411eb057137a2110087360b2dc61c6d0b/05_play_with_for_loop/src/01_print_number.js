b880808b139debe2e2514b507a8c3b644a759fe6
Gillian59 <sylviebaron5@gmail.com>
2020-09-10 17:45:39 +0200

    5exo3 termine

--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/01_print_number.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/01_print_number.js
@@ -1,1 +1,5 @@
 // Write on `stdout` each number from 0 to 100 with a for loop
+let number;
+for (let i=0;i<100;i++){
+  console.log(number);
+}
--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/03_iteration.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/03_iteration.js
@@ -1,4 +1,9 @@
 // ## Iteration on arrays
-//
+
 // -  Create an array called `literalDigits` from `zero` to `nine` where each array entry is a spelled-out number;
 // -  Using `length`, write on `stdout` each values of the table.
+const iteralDigits = [0,1,2,3,4,5,6,7,8,9]; 
+
+for (let i=0; i<iteralDigits.length;i++){
+  console.log(i);
+}
