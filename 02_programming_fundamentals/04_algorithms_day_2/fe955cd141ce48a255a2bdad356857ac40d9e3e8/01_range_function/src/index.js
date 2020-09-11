8750f2176bbc41633be186177d3d56a44aaa6850
Gillian59 <sylviebaron5@gmail.com>
2020-09-11 16:39:59 +0200

    exoRangefait

--- 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/01_range_function/src/index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/01_range_function/src/index.js
@@ -1,6 +1,31 @@
-function range() {
+function range(fromNumber, toNumber) {
   // Code the function here.
+  const result = [];
+  if (fromNumber < toNumber) {
+    for (i = fromNumber; i <= toNumber; i++) {
+     result.push(i);
+    }
+  }
+  else if (fromNumber > toNumber) {
+    for (i = fromNumber; i >= toNumber; i--) {
+     result.push(i);
+    }
+  }
+    
+  return result;
 }
+
+console.log(range(1,4));
+
+console.log(range(4,1));
 
 // Do not remove last lines, it is for tests
 module.exports = range;
+
+
+//const ascArray = range(1, 4);
+// => [1, 2, 3, 4]
+
+//const descArray = range(4, 1);
+// => [4, 3, 2, 1]
+
