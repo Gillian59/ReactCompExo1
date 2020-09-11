43298f9e8cdddc9d6c935bd3db15c64e862d1961
Gillian59 <sylviebaron5@gmail.com>
2020-09-11 16:56:17 +0200

    fizzBuzz

--- 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/03_fizz_buzz/src/index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/03_fizz_buzz/src/index.js
@@ -1,6 +1,27 @@
 function fizzBuzz() {
   // Code the function here.
+  const tab = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+  for (let i = 0 ; i < tab.length ; i++ ){
+
+    if (tab [i] % 3 === 0) {
+      tab [i] = "Fizz";
+    }
+    else if 
+    (tab [i] % 5 === 0) {
+      tab [i] = "Buzz";
+    }
+    else if 
+    ((tab [i] % 3 === 0) && (tab [i] % 5 === 0)) {
+      tab [i] = "FizzBuzz";
+    }
+
+  }
+  return console.log(tab);
 }
+
+fizzBuzz(5);
+
 
 // Leave the line below untouched for tests to work properly.
 module.exports = fizzBuzz;
