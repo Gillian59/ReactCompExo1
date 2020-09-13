3c64404c2a7830624289dc0ce8c91e16a2cb9f2d
Gillian59 <sylviebaron5@gmail.com>
2020-09-13 21:50:41 +0200

    dimsoir

--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/03_iteration.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/03_iteration.js
@@ -4,6 +4,7 @@
 // -  Using `length`, write on `stdout` each values of the table.
 const iteralDigits = [0,1,2,3,4,5,6,7,8,9]; 
 
+
 for (let i=0; i<iteralDigits.length;i++){
-  console.log(i);
+  console.log(iteralDigits[i]);
 }
