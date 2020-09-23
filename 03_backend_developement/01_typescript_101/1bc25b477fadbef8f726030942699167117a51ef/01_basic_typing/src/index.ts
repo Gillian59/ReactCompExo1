4edfe4f148ca601619c0a516c58dc8b7085ba4af
Gillian59 <sylviebaron5@gmail.com>
2020-09-23 14:31:02 +0200

    typescript1

---
+++ 03_backend_developement/01_typescript_101/1bc25b477fadbef8f726030942699167117a51ef/01_basic_typing/src/index.ts
@@ -0,0 +1,12 @@
+import machin from "./machin"
+
+const hello: string = "Sparta";
+const foo: number  = 12;
+const bar: number  = 8;
+const sumResult: number = foo + bar;
+const digits: number[] = [0,1,2,3,4,5,6,7,8,9];
+const isOpen: boolean = false;
+const empty: null  = null;
+
+// Leave the line below for tests to work properly.
+export { hello, foo, bar, sumResult, digits, isOpen, empty };
---
+++ 03_backend_developement/01_typescript_101/1bc25b477fadbef8f726030942699167117a51ef/02_typed_function/src/index.ts
@@ -0,0 +1,33 @@
+
+
+function handleTuple (tuple: [string, number]=["word", 2]): void {
+  
+  // Code the function here.
+
+  if (typeof [0])  {
+    console.log(tuple[0])
+  }
+  else (typeof[1]) {
+    console.log(tuple[1])
+  }
+  
+  
+};
+
+
+// const add = (a: number, b: number): number => a + b;
+
+//imprimer (word is a string) seulement apres avoir verifier que word etait bien un string respecte bien la condition
+
+// Leave the line below for tests to work properly.
+export {handleTuple};
+
+
+// let tuple: [string, number];
+// Initialize it
+//tuple = ["hello", 10]; // OK
+// Initialize it incorrectly
+//tuple = [10, "hello"]; // Error
+
+// Or as a one liner
+//const tuple: [string, number] = ["hello", 10];
